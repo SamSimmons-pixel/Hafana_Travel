a117f0e2a27fdd38d32523d20750bfbd1e5bfa31
WASD <simile64.exe@gmail.com>
2026-08-28 11:37:26 +0700

    noway this is works

--- Hafana-Backend/app/Http/Controllers/AppLandingController.php
+++ Hafana-Backend/app/Http/Controllers/AppLandingController.php
@@ -23,14 +23,14 @@ class AppLandingController extends Controller
         // URL will be: https://app.hafanatravel.com/images/app/screenshots/01-beranda.jpg
         //
         $screenshots = [
-            'mockupapp-img/mockup (1).jpg',
-            'mockupapp-img/mockup (2).jpg',
-            'mockupapp-img/mockup (3).jpg',
-            'mockupapp-img/mockup (4).jpg',
-            'mockupapp-img/mockup (5).jpg',
-            'mockupapp-img/mockup (6).jpg',
-            'mockupapp-img/mockup (7).jpg',
-            'mockupapp-img/mockup (8).jpg',
+            '/mockupapp-img/mockup (1).jpg',
+            '/mockupapp-img/mockup (2).jpg',
+            '/mockupapp-img/mockup (3).jpg',
+            '/mockupapp-img/mockup (4).jpg',
+            '/mockupapp-img/mockup (5).jpg',
+            '/mockupapp-img/mockup (6).jpg',
+            '/mockupapp-img/mockup (7).jpg',
+            '/mockupapp-img/mockup (8).jpg',
         ];
 
         // ── Download Link ───────────────────────────────────────────────────────
